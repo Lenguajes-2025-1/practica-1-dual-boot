de0eda641116ed5fe33054fe5e9610197cd1c3d4
wallsified <danielparedes@ciencias.unam.mx>
2024-10-09 13:21:00 -0600

    feat: Implementados parsers e interpretes AE y WAE

--- src/grammars.clj
+++ src/grammars.clj
@@ -10,55 +10,55 @@
 (defrecord WithG [assign body] WAE)
 
 (defn bindings [id value]
-    (if (and (symbol? id) (satisfies? WAE value))
-        (->Bindings id value)
-        (throw (IllegalArgumentException. "Error"))))
+  (if (and (symbol? id) (satisfies? WAE value))
+    (->Bindings id value)
+    (throw (IllegalArgumentException. "Error"))))
 
 (defn idG [i]
-    (if (symbol? i)
-        (->IdG i)
-        (throw (IllegalArgumentException. "Id debe ser un simbolo"))))
+  (if (symbol? i)
+    (->IdG i)
+    (throw (IllegalArgumentException. "Id debe ser un simbolo"))))
 
 (defn numG [n]
-    (if (number? n)
-        (->NumG n)
-        (throw (IllegalArgumentException. "Num debe ser un numero"))))
+  (if (number? n)
+    (->NumG n)
+    (throw (IllegalArgumentException. "Num debe ser un numero"))))
 
 (defn addG [i d]
-    (if (and (satisfies? WAE i)
-             (satisfies? WAE d))
-        (->AddG i d)
-        (throw (IllegalArgumentException. "Los parametros deben ser de tipo WAE"))))
+  (if (and (satisfies? WAE i)
+           (satisfies? WAE d))
+    (->AddG i d)
+    (throw (IllegalArgumentException. "Los parametros deben ser de tipo WAE"))))
 
 (defn subG [i d]
-    (if (and (satisfies? WAE i)
-             (satisfies? WAE d))
-        (->SubG i d)
-        (throw (IllegalArgumentException. "Los parametros deben ser de tipo WAE"))))
+  (if (and (satisfies? WAE i)
+           (satisfies? WAE d))
+    (->SubG i d)
+    (throw (IllegalArgumentException. "Los parametros deben ser de tipo WAE"))))
 
 (defn withG [bindings body]
-    (if (and 
-             (instance? Bindings bindings)
-             (satisfies? WAE body))
-        (->WithG bindings body)
-        (throw (IllegalArgumentException. "Los parametros deben ser de tipo Bindings WAE"))))
+  (if (and
+       (instance? Bindings bindings)
+       (satisfies? WAE body))
+    (->WithG bindings body)
+    (throw (IllegalArgumentException. "Los parametros deben ser de tipo Bindings WAE"))))
 
 (defn print_WAE [exp]
-    (cond 
-        (instance? IdG exp)
-        (str "(id " (:i exp) ")")
+  (cond
+    (instance? IdG exp)
+    (str "(id " (:i exp) ")")
 
-        (instance? NumG exp)
-        (str "(num " (:n exp) ")")
-        
-        (instance? AddG exp)
-        (str "(add " (print_WAE (:izq exp)) " " (print_WAE (:der exp)) ")")
-        
-        (instance? SubG exp)
-        (str "(sub " (print_WAE (:izq exp)) " " (print_WAE (:der exp)) ")")
-        
-        (instance? WithG exp)
-        (let [binding (:assign exp)]
-          (str "(with (" (:id binding) " " (print_WAE (:value binding)) ") " (print_WAE (:body exp)) ")"))))
+    (instance? NumG exp)
+    (str "(num " (:n exp) ")")
+
+    (instance? AddG exp)
+    (str "(add " (print_WAE (:izq exp)) " " (print_WAE (:der exp)) ")")
+
+    (instance? SubG exp)
+    (str "(sub " (print_WAE (:izq exp)) " " (print_WAE (:der exp)) ")")
+
+    (instance? WithG exp)
+    (let [binding (:assign exp)]
+      (str "(with (" (:id binding) " " (print_WAE (:value binding)) ") " (print_WAE (:body exp)) ")"))))
     
 
--- src/parser.clj
+++ src/parser.clj
@@ -2,11 +2,66 @@
   (:require [grammars :as wae]))
 
 (defn parser-AE
-    "Por implementar"
-    [exp]
-    ())
+  "Se parsea una expresión de tipo AE para relacionarla con su tipo correspondiente.
+
+  Params: 
+   - [exp]: La expresión de tipo AE a relacionar.
+
+   Uses:
+   - number?: Verifica si la expresión es un número.
+   - list?:  Verifica si la expresión es una lista de expresiónes.
+   - first:  Para obtener el primer elemento de la expresión.
+   - second: Para obtener el segundo elemento de la expresión.
+   - nth:    Para obtener el n-ésimo elemento de la expresión.
+
+   - Returns: 
+     Expresión AE relacionada con su tipo correspondiente.
+   "
+
+  [exp]
+  (cond
+    (number? exp) (wae/numG exp)
+    (list? exp) (case (first exp)
+                  + (wae/addG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+                  - (wae/subG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+                  ;; Si el operador no es reconocido se lanza la excepción.
+                  (throw (Exception. (str "Operador desconocido: " (first exp)))))
+    :else (throw (Exception. (str "Expresión Incorrecta: " exp)))))
 
 (defn parser-WAE
-    "Por implemntar"
-    [exp]
-    ())
+  "Se parsea una expresión de tipo WAE para relacionarla con su tipo correspondiente.
+  
+    Params: 
+     - [exp]: La expresión de tipo WAE a relacionar.
+  
+     
+     Uses:
+     - number?: Verifica si la expresión es un número.
+     - symbol?: Verifica si la expresión es un símbolo. 
+     - list?:  Verifica si la expresión es una lista de expresiónes.
+     - first:  Para obtener el primer elemento de la expresión.
+     - second: Para obtener el segundo elemento de la expresión.
+     - nth:    Para obtener el n-ésimo elemento de la expresión.
+  
+     - Returns: 
+       Expresión WAE relacionada con su tipo correspondiente.
+     "
+  [exp]
+  (cond
+    (number? exp) (wae/numG exp)
+    (symbol? exp) (wae/idG exp)
+    (list? exp) (case (first exp)
+                  + (wae/addG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+                  - (wae/subG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+                  with (let [binding (second exp)
+                             id (first binding)
+                             value (second binding)
+                             body (nth exp 2)]
+                         (wae/withG (wae/bindings id (parser-WAE value))
+                                    (parser-WAE body)))
+
+                  ;; Si el operador no es reconocido se lanza la excepción.
+                  (throw (Exception. (str "Operador desconocido: " (first exp)))))
+
+    ;; Si no es número, símbolo o lista, lanza una excepción
+    :else (throw (Exception. (str "Expresión Incorrecta: " exp)))))
